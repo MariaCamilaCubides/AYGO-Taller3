4cff073b25b40c60267b16e2791942c07a33d696
Maria Camila Cubides Martin <macacumar97@gmail.com>
2022-11-10 17:00:54 -0500

    Remove files outside folder

--- EC2/lib/user-data.sh
+++ EC2/lib/user-data.sh
@@ -5,6 +5,6 @@ yum install -y docker
 yum install -y git
 service docker start
 git clone https://github.com/MariaCamilaCubides/AYGO-Taller3.git
-cd /AYGO-Taller3
+cd /AYGO-Taller3/App
 docker build -t ui .
 docker run -d -p 8081:80 ui
